4be34f71b0cb57c9a85cbfd28657525240b7c67d
overtheline <aaronckeel@gmail.com>
2018-07-04 17:54:06 -0700

    client things for knn

--- public/app/app.tsx
+++ public/app/app.tsx
@@ -3,6 +3,12 @@ import {
 } from 'lodash';
 import * as React from 'react';
 
+import {
+	IIrisDatum,
+	IRawIrisDatum,
+} from '../../types';
+import { createChart } from './chart/scatter';
+
 interface IProps {
 	title: string;
 }
@@ -13,14 +19,31 @@ interface IState {
 }
 
 export default class App extends React.Component<IProps, IState> {
+	irisChartRef: React.RefObject<any>;
+
 	constructor(props: IProps) {
 		super(props);
+
+		this.irisChartRef = React.createRef();
 
 		bindAll(this, [
 			'handleSubmit',
 			'handleNameChange',
 			'handleTextChange',
+			'handlePredict',
+			'handleGetData',
 		]);
+	}
+
+	componentDidMount() {
+		fetch('/data/irisData')
+			.then((res) => {
+				return res.json();
+			})
+			.then((json: IRawIrisDatum[]) => {
+				console.log(json);
+				createChart(this.irisChartRef, '#iris-chart', json);
+			});
 	}
 
 	handleNameChange(event: any) {
@@ -33,30 +56,70 @@ export default class App extends React.Component<IProps, IState> {
 
 	handleSubmit(event: any) {
 		event.preventDefault();
-		return fetch('/data/quotes', {
-			body: JSON.stringify(this.state),
-			method: 'POST',
-		}).then((res) => {
-			console.log(res);
+		return fetch(`/data/quotes?name=${this.state.name}&text=${this.state.text}`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((json) => {
+			console.log(json);
 		});
+	}
+
+	handlePredict() {
+		return fetch(`/data/predictIris?petalLength=${0.5}&petalWidth=${3.4}&sepalLength=${1.7}&sepalWidth=${2.5}`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((json) => {
+			console.log(json);
+		});
+	}
+
+	handleGetData() {
+		return fetch('/data/irisData')
+		.then((res) => {
+			return res.json();
+		})
+		.then((json) => {
+			console.log(json);
+		});
+	}
+
+	renderIrisChart() {
+		return (
+			<div id="iris-chart" />
+		);
 	}
 
 	render() {
 		return (
 			<div>
 				<h1>{this.props.title}</h1>
-				<h2>This is index.html</h2>
-				<form onSubmit={this.handleSubmit}>
-					<label>
-						Name:
-						<input type="text" placeholder="name" onChange={this.handleNameChange} />
-					</label>
-					<label>
-						Text:
-						<input type="text" placeholder="quote" onChange={this.handleTextChange} />
-					</label>
-					<input type="submit" value="Submit" />
-				</form>
+				<section>
+					<div>
+						<input type="button" value="predict" onClick={this.handlePredict}/>
+					</div>
+					<div>
+						<input type="button" value="get data" onClick={this.handleGetData}/>
+					</div>
+					<div>
+						{this.renderIrisChart()}
+					</div>
+				</section>
+				<section>
+					<h2>This is index.html</h2>
+					<form onSubmit={this.handleSubmit}>
+						<label>
+							Name:
+							<input type="text" placeholder="name" onChange={this.handleNameChange} />
+						</label>
+						<label>
+							Text:
+							<input type="text" placeholder="quote" onChange={this.handleTextChange} />
+						</label>
+						<input type="submit" value="Submit" />
+					</form>
+				</section>
 			</div>
 		);
 	}
---
+++ public/app/chart/scatter/index.ts
@@ -0,0 +1,111 @@
+import * as d3 from 'd3';
+import * as React from 'react';
+
+import {
+	IIrisDatum,
+	IRawIrisDatum
+} from '../../../../types';
+
+const margin = {
+	bottom: 20,
+	left: 40,
+	right: 20,
+	top: 20,
+};
+
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+const xScale = d3.scaleLinear().range([0, width]);
+const yScale = d3.scaleLinear().range([height, 0]);
+
+const color = d3.schemeCategory10;
+
+const types = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'];
+
+function getTypeIndex(type: string) {
+	return types.indexOf(type);
+}
+
+const xAxis = d3.axisBottom(xScale);
+const yAxis = d3.axisLeft(yScale);
+
+let svg;
+
+export function createChart(ref: React.RefObject<any>, id: string, json: IRawIrisDatum[]) {
+	svg = d3.select(id).append('svg')
+			.attr('width', width + margin.left + margin.right)
+			.attr('height', height + margin.top + margin.bottom)
+		.append('g')
+			.attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+	const data: IIrisDatum[] = json.map(({
+		petalLength,
+		petalWidth,
+		sepalLength,
+		sepalWidth,
+		type,
+	}) => ({
+		petalLength: Number(petalLength),
+		petalWidth: Number(petalWidth),
+		sepalLength: Number(sepalLength),
+		sepalWidth: Number(sepalWidth),
+		type,
+	}));
+
+	const sepalWidthsExtent = d3.extent(data, (d) => d.sepalWidth) as [number, number];
+	const sepalLengthsExtent = d3.extent(data, (d) => d.sepalLength) as [number, number];
+
+	xScale.domain(sepalWidthsExtent).nice();
+	yScale.domain(sepalLengthsExtent).nice();
+
+	svg.append('g')
+			.attr('class', 'x axis')
+			.attr('transform', `translate(0, ${height})`)
+			.call(xAxis)
+		.append('text')
+			.attr('class', 'label')
+			.attr('x', width)
+			.attr('y', -6)
+			.style('text-anchor', 'end')
+			.text('Sepal Width (cm)');
+
+	svg.append('g')
+			.attr('class', 'y axis')
+			.call(yAxis)
+		.append('text')
+			.attr('class', 'label')
+			.attr('transform', 'rotate(-90)')
+			.attr('y', 6)
+			.attr('dy', '.71em')
+			.style('text-anchor', 'end')
+			.text('Sepal Length (cm)');
+
+	svg.selectAll('.dot')
+			.data(data)
+		.enter().append('circle')
+			.attr('class', 'dot')
+			.attr('r', 3.5)
+			.attr('cx', (d) => xScale(d.sepalWidth))
+			.attr('cy', (d) => yScale(d.sepalLength))
+			.style('fill', (d) => color[getTypeIndex(d.type)]);
+
+	const legend = svg.selectAll('.legend')
+			.data(types)
+		.enter().append('g')
+			.attr('class', 'legend')
+			.attr('transform', (d, i) => 'translate(0,' + i * 20 + ')');
+
+	legend.append('rect')
+			.attr('x', width - 18)
+			.attr('width', 18)
+			.attr('height', 18)
+			.style('fill', (d) => color[getTypeIndex(d)]);
+
+	legend.append('text')
+			.attr('x', width - 24)
+			.attr('y', 9)
+			.attr('dy', '.35em')
+			.style('text-anchor', 'end')
+			.text((d) => d);
+}
