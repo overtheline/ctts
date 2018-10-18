18b1a469780d7fa9faa741d9fabaaece51239ca9
overtheline <aaronckeel@gmail.com>
2018-10-18 11:01:48 -0700

    init stocks app

--- public/stocks/index.tsx
+++ public/stocks/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import App from './app';
+
 ReactDOM.render(
-	<div>Test</div>,
+	<App />,
 	document.getElementById('page')
 );
